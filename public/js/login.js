0ca0dfd5b4496c66f1f0c12a85b23bcdff3781ef
Jessica Yang <jessicayangg01@gmail.com>
2024-03-17 21:28:25 -0400

    working login and signup

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,4 @@
-// 1
+// connecting to first available data base
 const indexedDB =
   window.indexedDB ||
   window.mozIndexedDB ||
@@ -6,104 +6,57 @@ const indexedDB =
   window.msIndexedDB ||
   window.shimIndexedDB;
 
+// error unable to find indexdb
 if (!indexedDB) {
   console.log("IndexedDB could not be found in this browser.");
 }
 
-// 2
+// database start
 const request = indexedDB.open("Braindle_Database", 1);
 
+// error unable to connect to database
 request.onerror = function (event) {
     console.error("An error occurred with IndexedDB");
     console.error(event);
   };
 
 request.onupgradeneeded = function () {
-    //1
     const db = request.result;
-    //2
     // const store = db.createObjectStore("data_base", { keyPath: "id" });
     const store = db.createObjectStore("data_base", {keyPath:"username"});
-    //3
     store.createIndex("logins", "username", ["password"], { unique: true });
-    // 4
     // store.createIndex("scores", "username", ["score1", "score2"], {unique: false}); 
   };
 
 request.onsuccess = function () {
     console.log("Database opened successfully");
-    // const db = request.result;
-    // const transaction = db.transaction("logins", "readwrite");
-    // const store = transaction.objectStore("logins");
-    // const usernames = store.index("username");
-    // const datas = store.index("data");
-    // store.put({ id: 2, lastlogin: "Rehuhd", somethingelse: "Toyota" });
-    // store.put({ id: 3, lastlogin: "something", somethingelse: "hi test" });
-  
-    const db = request.result;
-  
-    // 1
-    const transaction = db.transaction("data_base", "readwrite");
-  
-    //2
-    const store = transaction.objectStore("data_base");
-    // const usernames = store.index("username");
-    // const datas = store.index("data");
-  
-    //3
-    // store.put({ id: 1, username: "Hi", password: "Test" });
-    // store.put({ id: 2, score1: "Hi", score2: "Test" });
-    // store.put({ id: 3, score1: "Hi", score2: "Test" });
-
-  
-    // // //4
-    const idQuery = store.get(1);
-    idQuery.onsuccess = function () {
-        console.log('idQuery', idQuery.result);
-      };
-    // // const usernmameQuery = usernames.getAll(["Red"]);
-    // // const dataQuery = datas.get(["Blue", "Honda"]);
-  
-    // // // 5
-    // // idQuery.onsuccess = function () {
-    // //   console.log('idQuery', idQuery.result);
-    // // };
-    // // colourQuery.onsuccess = function () {
-    // //   console.log('colourQuery', colourQuery.result);
-    // // };
-    // // colourMakeQuery.onsuccess = function () {
-    // //   console.log('colourMakeQuery', colourMakeQuery.result);
-    // // };
-  
-    // // // 6
-    // transaction.oncomplete = function () {
-    //   db.close();
-    // };
   };
 
 
 
-
-// OLD
-
 document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('login-submit').addEventListener('click', function() {
-        let username1 = document.getElementById("username-answer").value;
-        let password1 = document.getElementById(`password-answer`).value;
+        // getting variable inputs
+        let input_username = document.getElementById("username-answer").value;
+        let input_password = document.getElementById(`password-answer`).value;
         
+        // connect to database
         const db = request.result;
         const transaction = db.transaction("data_base", "readwrite");
         const store = transaction.objectStore("data_base");
         
-        
+        // search for username
         const index = store.index("logins");
-        const request1 = index.get(username1);
+        const request1 = index.get(input_username);
         request1.onsuccess = function() {
         const matching = request1.result;
+        
+        // finding username
         if (matching !== undefined) {
-            if (matching.password == password1){
-                alert("Success.");
+            // username found, check password
+            if (matching.password == input_password){
+                window.location.href = "../index.html";
             }
             else{
                 alert("wrong password");
@@ -116,17 +69,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         };
 
-        // store.put({ id: 5, username: username1, password: password1});
 
     });
 
     document.getElementById('signup-submit').addEventListener('click', function() {
-        // fs.writeFile("login_info.txt", document.getElementById('username-answer'), (err) =>{
-        //     if (err) throw err;
-        // })
 
-        let username1 = document.getElementById("username-answer").value;
-        let password1 = document.getElementById(`password-answer`).value;
+
+        let input_username = document.getElementById("username-answer").value;
+        let input_password = document.getElementById(`password-answer`).value;
         
         const db = request.result;
         const transaction = db.transaction("data_base", "readwrite");
@@ -134,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function () {
         
         
         const index = store.index("logins");
-        const request1 = index.get(username1);
+        const request1 = index.get(input_username);
         request1.onsuccess = function() {
         const matching = request1.result;
         if (matching !== undefined) {
@@ -143,8 +93,8 @@ document.addEventListener('DOMContentLoaded', function () {
             alert("This username is already taken.");
         } else {
             // No match was found.
-            console.log("did not find it");
-            store.put({username: username1, password: password1});
+            store.put({username: input_username, password: input_password});
+            window.location.href = "../index.html";
         }
         };
     });
