68c68f687f26ec8bccba868472e0b400b8831ef5
Rory <rory88_soccer@yahoo.ca>
2024-04-02 17:06:49 -0400

    Update db_conn.js

--- public/js/db_conn.js
+++ public/js/db_conn.js
@@ -192,6 +192,8 @@ export function getAllUserData() {
             reject(getRequest.error);
         };
     });
+}
+
 function getFormattedDate(date) {
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
