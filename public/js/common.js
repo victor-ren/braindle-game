dedab56ee9422a65b6785de6fc85e2d287fb04d5
Eric Wang <e.wang192@gmail.com>
2024-04-02 18:22:19 -0400

    updated puzzle refresh logic to maintain puzzle after refresh when backing out and re-entering site

--- public/js/common.js
+++ public/js/common.js
@@ -60,37 +60,3 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     function updateCountdown() {
-//         const now = new Date();
-//         const estOffset = now.getTimezoneOffset() * 60000 + (5 * 3600000);
-//         const estNow = new Date(now.getTime() + estOffset);
-
-//         const targetTime = new Date(estNow);
-//         targetTime.setUTCDate(estNow.getUTCDate() + (estNow.getUTCHours() >= 5 ? 1 : 0));
-//         targetTime.setUTCHours(5, 0, 0, 0);
-
-//         const timeDifference = targetTime - estNow;
-
-//         let hours = Math.floor(timeDifference / (1000 * 60 * 60));
-//         let minutes = Math.floor((timeDifference / (1000 * 60)) % 60);
-//         let seconds = Math.floor((timeDifference / 1000) % 60);
-
-//         hours = hours.toString().padStart(2, '0');
-//         minutes = minutes.toString().padStart(2, '0');
-//         seconds = seconds.toString().padStart(2, '0');
-
-//         const countdownTimer = document.getElementById('countdown-timer');
-//         if (countdownTimer) {
-//             countdownTimer.textContent = `${hours}:${minutes}:${seconds}`;
-//         }
-
-//         if (timeDifference <= 0) {
-//             clearInterval(intervalId);
-//             if (countdownTimer) {
-//                 countdownTimer.textContent = '00:00:00';
-//             }
-//         }
-//     }
